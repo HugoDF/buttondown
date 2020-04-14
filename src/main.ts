0384c5dbf5e34b00b1b2caa4bc84f9aad049bc7b
Hugo Di Francesco <HugoDF@users.noreply.github.com>
2020-04-14 16:41:10 +0100

    add drafts resource to export

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,12 @@
 import client from './lib/client';
 import ping from './ping';
+import * as drafts from './drafts';
 
 export default {
   // Alias from client
   setApiKey(apiKey: string): void {
     return client.setApiKey(apiKey);
   },
+  drafts,
   ping
 };
