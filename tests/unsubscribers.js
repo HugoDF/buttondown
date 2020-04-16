41fef84fb0ce5df349d377b248d3ee87f6c54fdd
Hugo Di Francesco <HugoDF@users.noreply.github.com>
2020-04-16 14:04:23 +0100

    fix lint + add missing HTTP error test on unsubscribers.list()

--- tests/unsubscribers.js
+++ tests/unsubscribers.js
@@ -1,3 +1,10 @@
+/**
+ * Unsubscribers failure/error scenarios
+ *
+ * Happy paths for List/Get tested
+ * in E2E suite smoke.js.
+ */
+
 import test from 'ava';
 import nock from 'nock';
 import buttondown from '../dist/main';
@@ -10,6 +17,24 @@ const nockOptions = {
     Authorization: 'Token super-secret-api-key'
   }
 };
+
+test('unsubscribers.list() - 401 - error', async (t) => {
+  nock('https://api.buttondown.email', nockOptions)
+    .get('/v1/unsubscribers')
+    .query({
+      page: 1
+    })
+    .reply(401, {});
+
+  const error = await t.throwsAsync(async () => {
+    await buttondown.unsubscribers.list();
+  });
+  t.is(error.message, 'Response code 401 (Unauthorized)');
+  t.is(error.url, 'https://api.buttondown.email/v1/unsubscribers');
+  t.is(error.method, 'GET');
+  t.is(error.payload, undefined);
+  t.deepEqual(error.query, {page: 1});
+});
 
 test('unsubscribers.get() - missing id', async (t) => {
   const error = await t.throwsAsync(async () => {
@@ -30,7 +55,10 @@ test('unsubscribers.get() - 404', async (t) => {
     await buttondown.unsubscribers.get('subscriber-id');
   });
   t.is(error.message, 'Response code 404 (Not Found)');
-  t.is(error.url, 'https://api.buttondown.email/v1/unsubscribers/subscriber-id');
+  t.is(
+    error.url,
+    'https://api.buttondown.email/v1/unsubscribers/subscriber-id'
+  );
   t.is(error.method, 'GET');
   t.is(error.payload, undefined);
 });
